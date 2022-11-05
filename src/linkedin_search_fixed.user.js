7c2ac43694422fa2cc5a2599ed2ad7c6c716c0d3
halt <halt9000@protonmail.com>
2022-11-05 09:33:45 +0300

    Script full refactored, added test https server

---
+++ src/linkedin_search_fixed.user.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         LinkedIn Job Search Usability Improvements
+// @namespace    http://tampermonkey.net/
+// @version      0.2.11.3
+// @description  Make it easier to review and manage job search results, with faster keyboard shortcuts, read post tracking, and blacklists for companies and jobs
+// @author       Bryan Chan, halt9k
+// @match        https://www.linkedin.com/jobs/search/*
+// @license      GNU GPLv3
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://localhost:8000/src/lsf_utility.js
+// @require      https://localhost:8000/src/lsf_persistent.js
+// @require      https://localhost:8000/src/lsf_selectors.js
+// @require      https://localhost:8000/src/lsf_navigation.js
+// @require      https://localhost:8000/src/lsf_ratings.js
+// @require      https://localhost:8000/src/lsf_display.js
+// @require      https://localhost:8000/src/linkedin_search_fixed.user.js
+// ==/UserScript==
+
+/** Event handler functions */
+LSF.prototype.keyListener = function(e) {
+	if (this.isSeacrhActive())
+		return;
+
+	/** Install key handlers to allow for keyboard interactions */
+	const KEY_HANDLER = {
+		'e': this.handleMarkRead, // toggle marking the active post as read
+		's': this.goToNext, // open the next visible job post
+		'w': this.goToPrevious, // open the previous visible job post
+		'h': this.toggleHidden, // toggle showing the hidden posts
+		'd': this.handleNextPage, // go to the next page
+		'a': this.handlePrevPage, // go to the previous page
+		'x': this.handleHidePost, // hide post forever,
+		'X': this.handleShowPost, // show post again
+		'y': this.handleHideCompany, // hide company forever
+		'Y': this.handleShowCompany, // show company again
+		'?': this.handlePrintDebug, // print debug information to the console
+	};
+
+	let handler = KEY_HANDLER[e.key];
+	if (!handler)
+		return;
+
+	handler.bind(this)();
+};
+
+LSF.prototype.jobListObserver = function(mutationsList, observer) {
+	// Use traditional 'for loops' for IE 11
+	for (let mutation of mutationsList) {
+		// const target = mutation.target;
+		if (mutation.type === 'childList') {
+			// console.log('Update queued, ' + mutation.type + ' ' + mutation.target.attributes);
+			this.queueUpdate();
+		} else if (mutation.type === 'attributes') {
+			//console.log('The ' + mutation.attributeName + ' attribute was modified.', target);
+		}
+	}
+};
+
+// Start observing the target node for configured mutations
+LSF.prototype.startObserve = function(jNode) {
+	this.observer = new MutationObserver(this.jobListObserver.bind(this));
+
+	console.log('Adding mutation observer');
+	const config = {attributes: false, childList: true, subtree: true};
+	this.observer.observe(this.getJobsList(), config);
+
+	window.addEventListener('keydown', this.keyListener.bind(this));
+};
+
+LSF.prototype.attach = function() {
+	console.log('Starting LinkedIn Job Search Usability Improvements');
+
+	/** Add styles to handle hiding */
+	GM_addStyle(`.${LSF.SELECTORS.CARDS_LIST_CLASS} { display: none }`);
+	GM_addStyle('.hidden { display: none }');
+	GM_addStyle('.read { opacity: 0.3 }');
+
+	this.startObserve();
+};
+
+const lsf = new LSF();
+
+// debug via window.lsf
+unsafeWindow.LSF = LSF;
+unsafeWindow.lsf = lsf;
+
+// waitForKeyElements uses $
+const $ = window.jQuery;
+waitForKeyElements(LSF.SELECTORS.CARDS_LIST_CONTAINER, lsf.attach.bind(lsf));
---
+++ src/lsf_persistent.js
@@ -0,0 +1,49 @@
+// Setup dictionaries to persist useful information across sessions
+LSF.StoredDictionary = class{
+	constructor(storageKey) {
+		this.storageKey = storageKey;
+		this.data = GM_getValue(storageKey) || {};
+		console.log('Initial data read from', this.storageKey, this.data);
+	}
+
+	get(key) {
+		return this.data[key];
+	}
+
+	set(key, value) {
+		this.data[key] = value;
+		GM_setValue(this.storageKey, this.data);
+	}
+
+	delete(key) {
+		delete this.data[key];
+		GM_setValue(this.storageKey, this.data);
+	}
+
+	getDictionary() {
+		return this.data;
+	}
+}
+
+LSF.prototype.createStorage = function() {
+	this.hiddenCompanies = new LSF.StoredDictionary('hidden_companies');
+	this.hiddenPosts = new LSF.StoredDictionary('hidden_posts');
+	this.readPosts = new LSF.StoredDictionary('read_posts');
+	this.highlightedWords = new LSF.StoredDictionary('highlighled_words');
+}
+
+// Handle requests to print debug information
+LSF.prototype.handlePrintDebug = function() {
+
+	const companies = this.hiddenCompanies.getDictionary();
+	console.log('Hidden companies', Object.keys(companies).length);
+	console.log(companies);
+
+	const posts = this.hiddenPosts.getDictionary();
+	console.log('Hidden posts', Object.keys(posts).length);
+	console.log(posts);
+
+	const read = this.readPosts.getDictionary();
+	console.log('Read posts', Object.keys(read).length);
+	console.log(read);
+}
