01f4ac6f5bb91f533714dc3c8aba1ef117e41d48
halt <halt9000@protonmail.com>
2022-11-07 06:38:25 +0300

    switch to local folder instead of server due to still existing cache problems

--- src/lsf_utility.js
+++ src/lsf_utility.js
@@ -23,12 +23,31 @@ LSF.prototype.triggerClick = function(node) {
 	this.triggerMouseEvent(node, 'click');
 };
 
-LSF.prototype.fuzzy_match = function(str, pattern) {
-	let cache = _.memoize(function(pattern) {
+LSF.prototype.fuzzy_match = function(text, pattern) {
+	//_.memoize(function(pattern) {
+	let cache = function(pattern) {
 		return new RegExp(pattern.split('').reduce(function(a, b) {
 			return a + '[^' + b + ']*' + b;
-		}));
-	});
+			}))
+	};
+	//});
 
-	return cache(pattern).test(str);
+	return cache(pattern).test(text);
 };
+
+LSF.fixFirefoxScriptCahche = function () {
+	let url = `https://localhost:8000/src/linkedin_search_fixed.user.js?ts=${(+new Date())}`
+	console.log('Cahche update attempt ' + url);
+
+	GM.xmlHttpRequest({
+	    method: "GET",
+	    url: url,
+	    onload: function(response) {
+	        let remoteScript = document.createElement('script')
+	        remoteScript.id = 'tm-dev-script'
+	        remoteScript.innerHTML = response.responseText
+	        document.body.appendChild(remoteScript)
+	    }
+	})
+}
+LSF.fixFirefoxScriptCahche();
--- test/run_server.bat
+++
@@ -1,2 +0,0 @@
-SET PATH=%PATH%;c:\ProgramData\Anaconda3\Library\bin\
-C:\ProgramData\Anaconda3\python.exe https_server\file_server.py
